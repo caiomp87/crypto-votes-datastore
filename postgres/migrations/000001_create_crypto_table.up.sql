f80735161c4bde0c727f725d2f7dafeaf1bcbe44
caio pereira <caiomp87@gmail.com>
2022-09-25 20:49:33 -0300

    create migration to create and drop crypto table

---
+++ postgres/migrations/000001_create_crypto_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS cryptos(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (50) NOT NULL,
+    network VARCHAR (50) NOT NULL,
+    upVotes INTEGER DEFAULT 0,
+    downVotes INTEGER DEFAULT 0
+)
